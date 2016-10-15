e0a4ff1115f4765c5e02c4b89762253c72e5ff08
Nguyen Van Dung <nguyen.van.dungb@framgia.com>
2016-10-15 20:23:29 +0700

    refs jjjjjj

--- BesterLive/AppDelegate.swift
+++ BesterLive/AppDelegate.swift
@@ -30,11 +30,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
   }
 
-  func applicationWillEnterForeground(application : UIApplication) {
+  func applicationWillEnterForeground(application : UIApplication){
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
   }
 
-  func applicationDidBecomeActive(application : UIApplication) {
+  func applicationDidBecomeActive(application : UIApplication){
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
   }
 
